16c6d95e21a8cab1cebfc2ff13aa7d5b54a054d7
madiyarzm <148677506+madiyarzm@users.noreply.github.com>
2025-04-11 01:27:49 +0900

    parses organic dates

--- search/initial_search_engine.py
+++ search/initial_search_engine.py
@@ -1,9 +1,12 @@
 from dotenv import load_dotenv
+from serpapi import GoogleSearch
 import os
 
+#loading environmental variables in safe way
 load_dotenv()
 api_key = os.getenv("SERPAPI_KEY")
 
+#creating search parameters in a dictionary
 test_parameters = {
     "engine": "google",
     "q": "A Fargo, North Dakota, man was arrested for clearing snow with a flamethrower.",
@@ -11,4 +14,21 @@ test_parameters = {
     "num": 5
 }
 
+#passing it to searching function 
+search_request = GoogleSearch(test_parameters)
 
+#parses JSON response, and returns in dictionary format
+results = search_request.get_dict()
+
+#getting organic results without adds, not needed data
+needed_results = results.get("organic_results", [])
+
+#looping through to get needed data:
+for res in needed_results:
+    print("Title", res.get("title"))
+    print("URL:", res.get("link"))
+    print("Snippet", res.get("snippet"))
+    print("Date:", res.get("date", "Not available"))
+
+    
+    print("-" * 40)
