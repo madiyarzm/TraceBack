1122c0259e24a37ec37fc1646f85bb0c9843de4b
madiyarzm <148677506+madiyarzm@users.noreply.github.com>
2025-04-11 00:46:19 +0900

    Creating test dictionary for search request
    Trying to test creating dict() for search request

--- search/initial_search_engine.py
+++ search/initial_search_engine.py
@@ -1,1 +1,14 @@
-#hi
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+api_key = os.getenv("SERPAPI_KEY")
+
+test_parameters = {
+    "engine": "google",
+    "q": "A Fargo, North Dakota, man was arrested for clearing snow with a flamethrower.",
+    "api_key": api_key,
+    "num": 5
+}
+
+
