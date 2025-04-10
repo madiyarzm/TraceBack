54a38769d2c66ddb4f27cc594f7a95dd0242c481
madiyarzm <148677506+madiyarzm@users.noreply.github.com>
2025-04-11 01:33:06 +0900

    wrapped it around function

--- search/initial_search_engine.py
+++ search/initial_search_engine.py
@@ -2,33 +2,49 @@ from dotenv import load_dotenv
 from serpapi import GoogleSearch
 import os
 
-#loading environmental variables in safe way
-load_dotenv()
-api_key = os.getenv("SERPAPI_KEY")
+def get_related_articles(article, num = 5):
+    #loading environmental variables in safe way
+    load_dotenv()
+    api_key = os.getenv("SERPAPI_KEY")
 
-#creating search parameters in a dictionary
-test_parameters = {
-    "engine": "google",
-    "q": "A Fargo, North Dakota, man was arrested for clearing snow with a flamethrower.",
-    "api_key": api_key,
-    "num": 5
-}
+    #creating search parameters in a dictionary
+    test_parameters = {
+        "engine": "google",
+        "q": article,
+        #"q": "A Fargo, North Dakota, man was arrested for clearing snow with a flamethrower.",
+        "api_key": api_key,
+        "num": num
+    }
 
-#passing it to searching function 
-search_request = GoogleSearch(test_parameters)
+    #passing it to searching function 
+    search_request = GoogleSearch(test_parameters)
 
-#parses JSON response, and returns in dictionary format
-results = search_request.get_dict()
+    #parses JSON response, and returns in dictionary format
+    results = search_request.get_dict()
 
-#getting organic results without adds, not needed data
-needed_results = results.get("organic_results", [])
+    #getting organic results without adds, not needed data
+    needed_results = results.get("organic_results", [])
 
-#looping through to get needed data:
-for res in needed_results:
-    print("Title", res.get("title"))
-    print("URL:", res.get("link"))
-    print("Snippet", res.get("snippet"))
-    print("Date:", res.get("date", "Not available"))
-
+    articles = []
     
-    print("-" * 40)
+    #looping through to get needed data:
+    for res in needed_results:
+        """
+        print("Title", res.get("title"))
+        print("URL:", res.get("link"))
+        print("Snippet", res.get("snippet"))
+        print("Date:", res.get("date", "Not available"))
+
+        
+        print("-" * 40)
+        """
+        
+        articles.append({
+            "title": res.get("title"),
+            "url": res.get("link"),
+            "snippet": res.get("snippet"),
+            "date": res.get("date", "Not available")  
+        })
+    
+    return articles
+
