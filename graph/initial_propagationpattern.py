675e6a941744920b454ace5c0222469932b67983
parkhaewon0617 <parkhaewon0617@gmail.com>
2025-05-27 00:28:35 +0900

    node styling

--- graph/initial_propagationpattern.py
+++ graph/initial_propagationpattern.py
@@ -12,5 +12,23 @@ G.add_edges_from([
     ("SiteD", "SiteF")    
 ])
 
-nx.draw(G, with_labels=True, arrows=True)
+out_degrees = dict(G.out_degree())
+
+node_colors = ["red" if node == "SiteA" else "lightgray" for node in G.nodes()]
+node_sizes = [ 300 + out_degrees[node]*400 for node in G.nodes()]
+edge_widths = [1 + out_degrees[edge[0]] for edge in G.edges()]
+# edge[0] means: the source node of the edge (where arrow starts)
+
+pos = nx.spring_layout(G, seed=42)
+nx.draw(
+    G, pos,
+    with_labels=True,
+    node_color=node_colors,
+    node_size=node_sizes,
+    width=edge_widths,
+    arrows=True   
+)
+
+
+plt.title("Fake News Propagation from SiteA")
 plt.show()
